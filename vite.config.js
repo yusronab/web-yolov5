24c448de4b8f07ff6969bdcc3f7405134931e9b4
yusronab <yusron.arly@gmail.com>
2023-07-04 20:40:47 +0700

    add base deployment

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
   },
-  base: "/yolov5-tfjs/",
+  base: "/"
+  // base: "/yolov5-tfjs/",
 });
